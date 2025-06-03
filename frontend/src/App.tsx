102e525ffb82176ddf38dd7d9cb2b9a4acca4013
勉強ユーザ１ <bk_takayuki@onotakaukinoMBP.local>
2025-06-03 23:30:43 +0900

    キャラクター選択処理

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
 import { Box, Image, SimpleGrid, Text } from "@chakra-ui/react";
-import { owList } from "./components/data/owlist";
+import { owList } from "./data/owlist";
+import { useState } from "react";
 
 export function App() {
+  //選択中のキャラ名の配列を状態として管理
+  const [selectedHeroes, setSelectedHeroes] = useState<string[]>([]);
+
+  //キャラをクリックした時の処理
+  const herosSelect = (heroName: string) => {
+    // 既に選択されている場合は選択を解除
+    setSelectedHeroes(
+      (prev) =>
+        prev.includes(heroName)
+          ? prev.filter((n) => n !== heroName) // 既に選択→解除
+          : [...prev, heroName] // 未選択→追加
+    );
+  };
   return (
+    // オーバーウォッチのキャラクター一覧を表示するコンポーネント
     <div style={{ padding: "20px" }}>
       <Text fontSize="2xl" fontWeight="bold" mb={4}>
         オーバーウォッチ キャラクター一覧
@@ -17,6 +32,10 @@ export function App() {
             overflow="hidden"
             p={4}
             textAlign="center"
+            borderColor={
+              selectedHeroes.includes(hero.name) ? "blue.400" : "gray.200"
+            }
+            onClick={() => herosSelect(hero.name)} // キャラをクリックした時の処理
           >
             <Image
               src={hero.image} // 画像のパス
@@ -24,7 +43,7 @@ export function App() {
               borderRadius="full" // 画像を丸く表示
               boxSize="80px" // 画像のサイズを指定
               mx="auto" // 中央揃え
-              mb={2}
+              mb={1}
             />
             <Text fontWeight="bold">{hero.name}</Text>
             <Text fontSize="sm" color="gray.500">
